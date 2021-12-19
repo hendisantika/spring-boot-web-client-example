0982941545693de8a90b852626fb19b322e4d72b
Hendi Santika <hendisantika@gmail.com>
2021-12-19 18:44:52 +0700

    #11 - Add init in Main App

--- src/main/java/com/hendisantika/SpringBootWebClientExampleApplication.java
+++ src/main/java/com/hendisantika/SpringBootWebClientExampleApplication.java
@@ -1,13 +1,26 @@
 package com.hendisantika;
 
+import com.hendisantika.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebClientExampleApplication {
+@Slf4j
+@AllArgsConstructor
+public class SpringBootWebClientExampleApplication implements CommandLineRunner {
+    private final UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebClientExampleApplication.class, args);
     }
 
+    @Override
+    public void run(final String... args) {
+        userService
+                .getUserByIdAsync("1")
+                .subscribe(user -> log.info("Get user async: {}", user));
+    }
 }
