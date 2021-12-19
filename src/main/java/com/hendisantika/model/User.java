f35789d0a012439fea3937b0483a787d2bdab707
Hendi Santika <hendisantika@gmail.com>
2021-12-19 18:40:24 +0700

    #3 - Add User Model

---
+++ src/main/java/com/hendisantika/model/User.java
@@ -0,0 +1,19 @@
+package com.hendisantika.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-web-client-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/12/21
+ * Time: 18.39
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+}
